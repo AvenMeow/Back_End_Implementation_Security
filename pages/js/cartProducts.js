d43d6d25240f2d0de2b12b98b2fa7c14b763a8eb
AvenMeow <maicolstiven.be@outlook.com>
2024-11-04 01:02:20 -0500

    Actualización del servidor

--- pages/js/cartProducts.js
+++ pages/js/cartProducts.js
@@ -1,6 +1,6 @@
 /* P3. back-end data in front-end */
 async function getProducts() {
-    const res = await fetch("http://localhost:3000/products"); // fetch envía una solicitud GET al servidor en el endpoint localhost:3000/products
-    const result = await res.json(); // Se procesa el cuerpo de la respuesta y se convierte a un objeto JavaScript (JSON)
-    return result; // Devuelve como el resultado los datos de productos obtenidos del servidor
+    const res = await fetch("http://localhost:3000/products"); //fetch envía una solicitud GET al servidor en el endpoint localhost:3000/products
+    const result = await res.json(); //Se procesa el cuerpo de la respuesta y se convierte a un objeto JavaScript (JSON)
+    return result; //Devuelve como el resultado los datos de productos obtenidos del servidor
 }
